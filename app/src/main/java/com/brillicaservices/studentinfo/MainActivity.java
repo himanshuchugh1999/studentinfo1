e72f77f6631a0b0e0c74563845c373ad53b3c5a9
Himanshu Chugh <37638180+himanshuchugh1999@users.noreply.github.com>
2018-07-05 11:37:11 +0530

    changes

--- app/src/main/java/com/brillicaservices/studentinfo/MainActivity.java
+++ app/src/main/java/com/brillicaservices/studentinfo/MainActivity.java
@@ -5,13 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     ArrayList<student> studentClassArrayList = new ArrayList();
     Button btn;
     EditText nn;
@@ -20,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
     EditText cc;
     Button b;
     TextView displayStudentsResultTV;
+    String collegeName = "";
+    Spinner spinnerCollegeNames;
+
+    /*
+    * Creating a string of array of colleges*/
+    String collegeNames[] = {"Select college name","DIT", "Graphic Era", "HNB"};
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +40,28 @@ public class MainActivity extends AppCompatActivity {
         cc = (EditText) findViewById(R.id.c);
         b=(Button) findViewById(R.id.button);
         displayStudentsResultTV = findViewById(R.id.textView);
+
+        spinnerCollegeNames = findViewById(R.id.college_id_spinner);
+
+        /*
+        * Using setOnItemSelectedListener on spinner object
+        * and giving it the context - this, meaning current activity*/
+        spinnerCollegeNames.setOnItemSelectedListener(this);
+
+        /*
+        * Creating an arrayAdapter object and passing 3 different arguments
+        * i.e. context, layout, array*/
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, collegeNames);
+
+        /*
+        * using the spinner's setAdapter method to update it's adapter*/
+        spinnerCollegeNames.setAdapter(arrayAdapter);
+
+        /*
+        * setPrompt is select on spinner to just give the refernce that the
+        * first object of array is only a label.*/
+        spinnerCollegeNames.setPrompt(collegeNames[0]);
+
         btn.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
@@ -38,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 String address = aa.getText().toString();
                 long phone = Long.parseLong(pp.getText().toString());
                 String college = cc.getText().toString();
-                boolean add = studentClassArrayList.add(new student(namevalue, address, phone, college));
+                boolean add = studentClassArrayList.add(new student(namevalue, address, phone, collegeName));
             }
         });
         b.setOnClickListener(new View.OnClickListener() {
@@ -71,5 +102,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        /*
+        * getting college name from the list of colleges.
+        * It will be updated only on the basis of array.*/
+        collegeName = collegeNames[i];
     }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
 
